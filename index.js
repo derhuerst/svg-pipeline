b82faee1e73ee1498908d9cd4dcf79c8d8166bfb
Jannis R <mail@jannisr.de>
2017-02-08 22:49:19 +0100

    parse, run through middleware, stringify

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 'use strict'
 
-// todo
+const createParser = require('html-to-vdom')
+const VNode = require('virtual-dom/vnode/vnode')
+const VText = require('virtual-dom/vnode/vtext')
+const middl = require('middl')
+const stringify = require('vdom-to-html')
+
+const parse = createParser({VNode, VText})
+
+const createPipeline = () => {
+  const pipeline = middl()
+
+  const run = (svg) =>
+    pipeline.run({
+      tree: parse(svg)[0]
+    }, {})
+    .then(({tree}) => stringify(tree))
+
+  const out = Object.create(pipeline)
+  out.run = run
+  return out
+}
+
+module.exports = createPipeline
