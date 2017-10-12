7464535dfae6ced212c2362a733777132073e78f
Jannis R <mail@jannisr.de>
2017-10-12 20:31:24 +0200

    fix lib/hrefs :bug:, tests :white_check_mark:

---
+++ test/lib/hrefs.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const h = require('virtual-dom/virtual-hyperscript/svg')
+const test = require('tape')
+
+const hrefs = require('../../lib/hrefs')
+
+const tree = h('svg', {
+	xmlns: 'http://www.w3.org/2000/svg',
+	'xmlns:xlink': 'http://www.w3.org/1999/xlink'
+}, [
+	h('use', {href: '#foo', x: '10', y: '10'}),
+	h('use', {href: 'bar.svg#baz', x: '10', y: '10'}),
+	h('use', {'xlink:href': '#qux', x: '10', y: '10'})
+])
+
+test('lib/hrefs', (t) => {
+	t.plan(1)
+	t.deepEqual(hrefs(tree).sort(), [
+		'#foo', '#qux', 'bar.svg#baz'
+	])
+})
