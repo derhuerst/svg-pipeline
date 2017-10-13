501dc6c64b98cabaf2389fcf96c8e261883598fb
Jannis R <mail@jannisr.de>
2017-10-13 15:47:30 +0200

    bugfixes :bug: :white_check_mark:

--- test/lib/hrefs.js
+++ test/lib/hrefs.js
@@ -1,22 +1,28 @@
 'use strict'
 
 const h = require('snabbdom/h').default
+const snabby = require('snabby')
 const test = require('tape')
 
 const hrefs = require('../../lib/hrefs')
 
-const tree = h('svg', {
-	xmlns: 'http://www.w3.org/2000/svg',
-	'xmlns:xlink': 'http://www.w3.org/1999/xlink'
+const tree1 = h('svg', {
+  xmlns: 'http://www.w3.org/2000/svg',
+  'xmlns:xlink': 'http://www.w3.org/1999/xlink'
 }, [
-	h('use', {href: '#foo', x: '10', y: '10'}),
-	h('use', {href: 'bar.svg#baz', x: '10', y: '10'}),
-	h('use', {'xlink:href': '#qux', x: '10', y: '10'})
+  h('use', {href: '#foo', x: '10', y: '10'}),
+  h('use', {href: 'bar.svg#baz', x: '10', y: '10'}),
+  h('use', {'xlink:href': '#qux', x: '10', y: '10'})
 ])
 
+const tree2 = snabby `
+<svg xmlns="http://www.w3.org/2000/svg">
+  <use href="#foo" />
+</svg>`
+
 test('lib/hrefs', (t) => {
-	t.plan(1)
-	t.deepEqual(hrefs(tree).sort(), [
-		'#foo', '#qux', 'bar.svg#baz'
-	])
+  t.plan(2)
+
+  t.deepEqual(hrefs(tree1).sort(), ['#foo', '#qux', 'bar.svg#baz'])
+  t.deepEqual(hrefs(tree2).sort(), ['#foo'])
 })
--- test/lib/ids.js
+++ test/lib/ids.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const h = require('snabbdom/h').default
+const snabby = require('snabby')
 const test = require('tape')
 
 const ids = require('../../lib/ids')
 
-const tree = h('svg', {xmlns: 'http://www.w3.org/2000/svg'}, [
+const tree1 = h('svg', {xmlns: 'http://www.w3.org/2000/svg'}, [
 	h('defs', [ // must ignore defs
 		h('path', {id: 'foo', d: 'M10 10 h10'})
 	]),
@@ -17,7 +18,14 @@ const tree = h('svg', {xmlns: 'http://www.w3.org/2000/svg'}, [
 	])
 ])
 
+const tree2 = snabby `
+<svg xmlns="http://www.w3.org/2000/svg">
+	<path id="foo" d="M10 10 h10" />
+</svg>`
+
 test('lib/ids', (t) => {
-	t.plan(1)
-	t.deepEqual(ids(tree).sort(), ['bar', 'baz'])
+	t.plan(2)
+
+	t.deepEqual(ids(tree1).sort(), ['bar', 'baz'])
+	t.deepEqual(ids(tree2).sort(), ['foo'])
 })
