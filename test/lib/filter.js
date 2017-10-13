4b1072ee062a832a2ff401849ebecfa43eb15978
Jannis R <mail@jannisr.de>
2017-10-13 14:58:05 +0200

    filter: onLeave hook

--- test/lib/filter.js
+++ test/lib/filter.js
@@ -6,31 +6,31 @@ const test = require('tape')
 const filter = require('../../lib/filter')
 
 const tree = h('svg', {xmlns: 'http://www.w3.org/2000/svg'}, [
-	h('defs', [
-		h('path', {id: 'foo', d: 'M10 10 h10'})
-	]),
-	h('g', {id: 'bar'}, [
-		h('circle', {r: '10', cx: '10', cy: '10'})
-	]),
-	h('circle', {r: '5', cx: '5', cy: '5'})
+  h('defs', [
+    h('path', {id: 'foo', d: 'M10 10 h10'})
+  ]),
+  h('g', {id: 'bar'}, [
+    h('circle', {r: '10', cx: '10', cy: '10'})
+  ]),
+  h('circle', {r: '5', cx: '5', cy: '5'})
 ])
 
 const predicate = (node, parent) => {
-	if (node.sel === 'path' && parent.sel === 'defs') return false
-	if (node.sel === 'circle' && parent.sel === 'svg') return false
-	return true
+  if (node.sel === 'path' && parent.sel === 'defs') return false
+  if (node.sel === 'circle' && parent.sel === 'svg') return false
+  return true
 }
 
 const filtered = h('svg', {xmlns: 'http://www.w3.org/2000/svg'}, [
-	h('defs', []),
-	h('g', {id: 'bar'}, [
-		h('circle', {r: '10', cx: '10', cy: '10'})
-	])
+  h('defs'),
+  h('g', {id: 'bar'}, [
+    h('circle', {r: '10', cx: '10', cy: '10'})
+  ])
 ])
 
 test('lib/filter', (t) => {
-	t.plan(1)
+  t.plan(1)
 
-	filter(tree, predicate) // mutates
-	t.deepEqual(tree, filtered)
+  filter(tree, predicate) // mutates
+  t.deepEqual(tree, filtered)
 })
